3de4cc1cfa9f3e002fb017c48dd7b6a14a56d607
casheww <51793722+casheww@users.noreply.github.com>
2021-04-28 21:03:00 +0100

    basic edge avoidance

--- main/main.ino
+++ main/main.ino
@@ -7,43 +7,60 @@
 #define LED_BUILTIN 1
 
 
+/*
+ * SPECIAL NOTES:
+ * - 180 seems to be the motor power required to kick it off moving.
+ * - somewhere between 120 and 160 is a cutoff point where the power is not enough to move the weight of the robot
+ */
+
+
+
 void setup() {
   Serial.begin(115200);   // setup debugging connection back to a computer
   setupSpacial();         // setup spacial sensors and pins and whatnot, y'know
   setupMotors();          // setup motor pin modes
 }
 
+
 void loop() {
-  setLED(0, true);
-  delay(500);
-  setLED(1, true);
-  delay(500);
-  setLED(0, false);
-  setLED(1, false);
-  delay(500);
+  crossBridge();
+}
+
+
+void crossBridge() {
+  bool success = false;
+  bool backedThisLoop = true;
   
-  int tofDist = getTof();
-  Serial.print(tofDist);
-  Serial.println();
+  while (!success) {
+    // check left tracker sensor
+    if (!getTracker(0)) {
+      backedThisLoop = true;
+      stopDriveMotors();
+      delay(100);
+      setDriveMotors(-180, -180);
+      delay(200);
+      setDriveMotors(-140, 0);
+      delay(1000);
+      stopDriveMotors();
+    }
+    // check right tracker sensor
+    else if (!getTracker(1)) {
+      backedThisLoop = true;
+      stopDriveMotors();
+      delay(100);
+      setDriveMotors(-180, -180);
+      delay(200);
+      setDriveMotors(0, -140);
+      delay(1000);
+      stopDriveMotors();
+    }
 
-  Serial.print("left tracker:");
-  Serial.print(getTracker(0));
-  Serial.println();
-
-  Serial.print("right tracker:");
-  Serial.print(getTracker(1));
-  Serial.println();
-
-  Serial.print("usound:");
-  Serial.print(getUsound());
-  Serial.println();
-
-  setDriveMotors(180, -180);
-  delay(1000);
-  stopDriveMotors();
-  delay(1000);
-  setDriveMotors(-180, 180);
-  delay(1000);
-  stopDriveMotors();
-  
+    // set motors forward again if safe to move forward
+    if (backedThisLoop && getTracker(0) && getTracker(1)) {
+      setDriveMotors(180, 180);
+      delay(300);
+      setDriveMotors(160, 160);
+      backedThisLoop = false;
+    }
+  }
 }
